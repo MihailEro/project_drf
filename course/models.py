29a3a6b844b8ad3e47d4160903bb13d76928b602
MihailEro <mihailerohin235@gmail.com>
2023-10-26 17:43:08 +0300

    Write model Subscribe

--- course/models.py
+++ course/models.py
@@ -21,3 +21,15 @@ class Course(models.Model):
     class Meta:
         verbose_name = "курс"
         verbose_name_plural = "курсы"
+
+
+class Subscribe(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='студент')
+
+    def __str__(self):
+        return f'{self.course}:{self.user}'
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
